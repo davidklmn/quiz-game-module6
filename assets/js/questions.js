c9cdc2bc97b948d2df59dc9fc8561df1c4f7921c
davidklmn <davidkelemen88@gmail.com>
2023-01-16 16:45:22 +0000

    Questions added

---
+++ assets/js/questions.js
@@ -0,0 +1,44 @@
+var questionArr = [
+  {
+    question: "Commonly used datatypes DO NOT include:",
+    1: "1. Strings",
+    2: "2. Boolean",
+    3: "3. Alerts",
+    4: "4. Numbers",
+    correct: "3",
+  },
+  {
+    question: "The condintion in an if / else statement enclosed with _______.",
+    1: "1. Quotes",
+    2: "2. Curly brackets",
+    3: "3. Parenthesis",
+    4: "4. Square brackets",
+    correct: "3",
+  },
+  {
+    question: "Arrays in JavaScript can be used to store _______.",
+    1: "1. Numbers and strings",
+    2: "2. Other arrays",
+    3: "3. Booleans",
+    4: "4. All of the above",
+    correct: "4",
+  },
+  {
+    question:
+      "String values must be enclosed within ______ when being assigned to variables.",
+    1: "1. Commas",
+    2: "2. Curly brackets",
+    3: "3. Quotes",
+    4: "4. Parenthesis",
+    correct: "3",
+  },
+  {
+    question:
+      "A very useful tool used during development and debugging for printing content to the debugger is:",
+    1: "1. JavaScript",
+    2: "2. Terminal/bash",
+    3: "3. For loops",
+    4: "4. console.log",
+    correct: "4",
+  },
+];
