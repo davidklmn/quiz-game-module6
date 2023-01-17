60df354a41793a88f2f36dfd6e5322890c728110
davidklmn <davidkelemen88@gmail.com>
2023-01-17 13:13:37 +0000

    Rewrote the whole JS as the first one confused me totally :D

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,31 +1,50 @@
 var startDiv = document.getElementById("start-screen");
+var endDiv = document.getElementById("end-screen");
 var quiz = document.getElementById("questions");
 var quizQuestion = document.getElementById("question-title");
 var answers = document.getElementById("choices");
 var questionButton = document.createElement("button");
-
-var currentQuiz = 0;
+var finalScore = document.getElementById("final-score");
 var time = document.getElementById("time");
+var questionNumber = 0;
+var currentQuestion = questionObj[questionNumber];
 
-document.getElementById("start").addEventListener("click", startQuiz);
-
-function startQuiz() {
+document.getElementById("start").addEventListener("click", function () {
+  displayQuestion();
   startTimer();
+});
 
+function displayQuestion() {
   quiz.className = "show";
-  startDiv.style.display = "none";
-
-  var currentQuestion = questionObj[currentQuiz];
+  startDiv.className = "hide";
   var questionArr = Object.values(currentQuestion);
-  var button = document.createElement("button");
-
   quizQuestion.innerText = currentQuestion.question;
 
   for (i = 0; i < 4; i++) {
-    var button = document.createElement("button");
-    answers.appendChild(button);
-    button.innerText = questionArr[i];
+    var choice = document.createElement("button");
+    answers.appendChild(choice);
+    choice.innerText = questionArr[i];
+    choice.addEventListener("click", checkChoice);
   }
+}
+
+function checkChoice(e) {
+  var selected = e.target;
+  if (selected.textContent === currentQuestion.correct) {
+    clearCurrent();
+    questionNumber++;
+    displayQuestion();
+  } else {
+    time.innerText -= 10;
+    clearCurrent();
+    questionNumber++;
+    displayQuestion();
+  }
+}
+
+function clearCurrent() {
+  questionArr = [];
+  quizQuestion.innerText = " ";
 }
 
 function startTimer() {
@@ -33,11 +52,19 @@ function startTimer() {
   var timerInterval = setInterval(function () {
     time.innerText--;
 
-    if (time.innerText == 0) {
+    if (time.innerText <= 0 || questionNumber > 5) {
       clearInterval(timerInterval);
       ending();
+      localStorage.setItem("score", time.innerText);
+    }
+    if (time.innerText < 0) {
+      time.innerText = 0;
     }
   }, 1000);
 }
 
-function ending() {}
+function ending() {
+  quiz.className = "hide";
+  endDiv.className = "show";
+  finalScore.textContent = localStorage.getItem("score");
+}
---
+++ assets/js/logic2.js
@@ -0,0 +1,89 @@
+// Variables
+var startButton = document.getElementById("start");
+var startScreen = document.getElementById("start-screen");
+var questionDiv = document.getElementById("questions");
+var questionTitle = document.getElementById("question-title");
+var endScreen = document.getElementById("end-screen");
+var choices = document.getElementById("choices");
+var currentQuestion;
+var questionIndex = 0;
+// Click licstener for start button which starts the quiz game
+startButton.addEventListener("click", startQuiz);
+// Function to start the quiz game
+function startQuiz() {
+  startScreen.className = "hide";
+  questionDiv.className = "show";
+  nextQuestion();
+  startTimer();
+}
+// Function for showing next question
+function nextQuestion() {
+  clearQuestion();
+  currentQuestion = questionObj[questionIndex];
+  showQuestion(currentQuestion);
+}
+function showQuestion(question) {
+  questionTitle.innerText = question.question;
+  for (i = 0; i < currentQuestion.answers.length; i++) {
+    var button = document.createElement("button");
+    button.innerText = currentQuestion.answers[i];
+    choices.appendChild(button);
+    button.addEventListener("click", selectedAnswer);
+  }
+}
+// Function resets the question and clears the questionDiv
+function clearQuestion() {
+  questionTitle.innerText = " ";
+  while (choices.firstChild) {
+    choices.removeChild(choices.firstChild);
+  }
+}
+
+// Function for checking the answer if its correct or not
+function selectedAnswer(e) {
+  if (questionIndex >= 4 || time.innerText == 0) {
+    ending();
+    return;
+  } else {
+    var selectedButton = e.target;
+    if (selectedButton.textContent === currentQuestion.correct) {
+      correctAnswer();
+    } else {
+      incorrectAnswer();
+    }
+  }
+}
+// If the answer is correct
+function correctAnswer() {
+  clearQuestion();
+  questionIndex++;
+  nextQuestion();
+}
+// If the answer is incorrect
+function incorrectAnswer() {
+  time.innerText -= 15;
+  clearQuestion();
+  questionIndex++;
+  nextQuestion();
+}
+// Timer function
+function startTimer() {
+  time.innerText = 60;
+  var timerInterval = setInterval(function () {
+    time.innerText--;
+
+    if (time.innerText <= 0) {
+      clearInterval(timerInterval);
+      localStorage.setItem("score", time.innerText);
+    }
+    if (time.innerText < 0) {
+      clearInterval(timerInterval);
+      time.innerText = 0;
+    }
+  }, 1000);
+}
+//Game ending
+function ending() {
+  clearQuestion();
+  endScreen.className = "show";
+}
